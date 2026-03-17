0bd83e47928cae256bd25dbd511c4f5035fc7b31
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2026-03-18 02:00:20 +0300

    16

---
+++ FlightBooking/Areas/Admin/Controllers/FlightsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class FlightsController : Controller
+    {
+        public IActionResult FlightList()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateFlight()
+        {
+            return View();
+        }
+    }
+}
---
+++ FlightBooking/Areas/Admin/Controllers/LayoutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class LayoutController : Controller
+    {
+        public IActionResult AdminLayout()
+        {
+            return View();
+        }
+    }
+}
