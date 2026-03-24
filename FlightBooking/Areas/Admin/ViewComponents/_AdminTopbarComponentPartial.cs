ca158097b96db782576c4388b4793817dd0ae691
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2026-03-24 21:19:24 +0300

    #17 - Admin Sidebar Türkçeleştirme & Layout Tamamlama

---
+++ FlightBooking/Areas/Admin/ViewComponents/_AdminTopbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.ViewComponents
+{
+    public class _AdminTopBarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
