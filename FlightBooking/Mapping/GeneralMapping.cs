5962a916d139ed2daa69041a9cfad7f1a8911165
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2026-03-24 21:51:29 +0300

    #19 - DTO Oluşturma & AutoMapper Kullanımı

---
+++ FlightBooking/Mapping/GeneralMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FlightBooking.DTOs.FlightDTOs;
+using FlightBooking.Entities;
+
+namespace FlightBooking.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Flight, CreateFlightDTO>().ReverseMap();
+            CreateMap<Flight, ResultFlightDTO>().ReverseMap();
+            CreateMap<Flight, UpdateFlightDTO>().ReverseMap();
+            CreateMap<Flight, GetFlightByIdDTO>().ReverseMap();
+        }
+    }
+}
