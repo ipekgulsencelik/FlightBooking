2fad47d871ffeae17a39403597514017416ea1f3
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2026-03-27 21:50:52 +0300

    28 - Create Booking & Passenger Sayfası

---
+++ FlightBooking/DTOs/PassengerDTOs/CreatePassengerDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FlightBooking.DTOs.PassengerDTOs
+{
+    public class CreatePassengerDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Gender { get; set; } // Male, Female
+        public string PassengerType { get; set; } // Adult, Child, Infant
+    }
+}
