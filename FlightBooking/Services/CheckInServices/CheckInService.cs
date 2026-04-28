a31f4ffd3c0dfe5d8928b0d590b79a78e6d1e472
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2026-04-28 22:25:52 +0300

    #49 - Check-In Service ve Metot Altyapısı

---
+++ FlightBooking/Services/CheckInServices/CheckInService.cs
@@ -0,0 +1,27 @@
+﻿using FlightBooking.DTOs.CheckInDTOs;
+using FlightBooking.Entities;
+using FlightBooking.Settings;
+using MongoDB.Driver;
+
+namespace FlightBooking.Services.CheckInServices
+{
+    public class CheckInService : ICheckInService
+    {
+        private readonly IMongoCollection<Booking> _bookingCollection;
+        private readonly IMongoCollection<CheckIn> _checkInCollection;
+
+        public CheckInService(IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _bookingCollection = database.GetCollection<Booking>(settings.BookingCollectionName);
+            _checkInCollection = database.GetCollection<CheckIn>(settings.CheckInCollectionName);
+        }
+
+        public Task CompleteCheckInAsync(CompleteCheckInDTO completeCheckInDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FlightBooking/Services/CheckInServices/ICheckInService.cs
@@ -0,0 +1,9 @@
+﻿using FlightBooking.DTOs.CheckInDTOs;
+
+namespace FlightBooking.Services.CheckInServices
+{
+    public interface ICheckInService
+    {
+        Task CompleteCheckInAsync(CompleteCheckInDTO completeCheckInDTO);
+    }
+}
